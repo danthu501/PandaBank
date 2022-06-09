760143f3e7a034297865861cf6828591c5d9923e
Daniel Thulin <daniel_thulin@outlook.com>
2022-06-09 13:05:45 +0200

    Removed copied code

--- PandaBank.Test/UnitTest1.cs
+++ PandaBank.Test/UnitTest1.cs
@@ -38,24 +38,71 @@ namespace PandaBank.Test
 
             customer.AddAccounts(accounts);
 
-            var Test = customer.ListOfAccounts[0];
-            var actual = accounts;
+            var Test = customer.ListOfAccounts[0]._Balance;
+            var actual = 300;
 
             Assert.AreEqual(Test, actual);
         }
         [TestMethod]
-        public void withdraw_Money_Test()
+        public void create_Customer()
         {
-            Accounts accounts = new Accounts("Testkonto", 500, "SEK");
-            Customer customer = new Customer("Test", "test1234");
-            customer.AddAccounts(accounts);
+            Admin admin = new Admin();
+            Customer customer = new Customer("Test", "Test1234");
+            admin.ListOfCustomers.Add(customer);
+
+            var test = admin.ListOfCustomers[0];
+            var actual = customer.UserName;
+            Assert.AreEqual(test.UserName, actual);
 
             
-            var test = customer.IntrestAmount(accounts);
-
-
-            Assert.AreEqual(test, accounts._Balance);
+            
            
+
+        }
+
+        [TestMethod]
+        public void ExchangeRate_Test_When_Sending_100_SEK_And_Reciver_100_SEK()
+        {
+            Customer customer = new Customer("Test", "Test1234");
+            Accounts account1 = new Accounts("Testkonto", 300, "SEK");
+            Accounts account2 = new Accounts("Testkonto", 300, "SEK");
+
+            customer.AddAccounts(account1);
+            customer.AddAccounts(account2);
+
+
+            decimal test = customer.ExchangeRate(customer.ListOfAccounts[0], customer.ListOfAccounts[1], 100);
+            decimal actual = 100;
+
+
+            Assert.AreEqual(test, actual);
+
+        }
+        [TestMethod]
+        public void ExchangeRate_Test_When_Sending_100_GBP_And_Reciver_In_SEK()
+        {
+            //Arrange
+            Customer customer = new Customer("Test", "Test1234");
+            Accounts account1 = new Accounts("Testkonto", 300, "GBP");
+            Accounts account2 = new Accounts("Testkonto", 300, "SEK");
+
+            customer.AddAccounts(account1);
+            customer.AddAccounts(account2);
+
+            Accounts FirstAccount = customer.ListOfAccounts[0];
+            Accounts SecondAccount = customer.ListOfAccounts[1];
+
+            var ExchangeRate = LoginUser.currencyChange[2];
+            
+            //Act
+            
+
+            decimal test = customer.ExchangeRate(FirstAccount, SecondAccount, 100);
+            decimal actual = 100 / ExchangeRate; 
+              
+
+            //Assert
+            Assert.AreEqual(test, actual);
 
         }
     }
